2eb7ebbf1646399ac1d06576563e29f8820105aa
dzzlr <githubdazzler@gmail.com>
2022-03-29 15:08:32 +0700

    setup new dev-kit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +15,4 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
-Route::group(['middleware'=>'auth'], function() {
-    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-    Route::get('/product/edit', [ProductController::class, 'edit'])->name('product.edit');
-
 });
