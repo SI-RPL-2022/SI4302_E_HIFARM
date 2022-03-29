4352cb08aeadf6bc5f453aa707a6a97214befbeb
dzzlr <githubdazzler@gmail.com>
2022-03-29 11:08:38 +0700

    setup dev-kit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,49 @@
+<footer class="footer mt-auto py-3 bg-white">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-sm-5">
+                <div>
+                    <h5 class="fw-bold">PT. Hifarm Satu Nusantara</h5>
+                    <p>Perdagangan Peternakan Terintegrasi. Misi kami adalah menjadikan peternakan sebagai mata
+                        pencaharian yang
+                        lebih baik bagi semua orang dengan menciptakan ekosistem peternakan yang adil dan
+                        berkelanjutan.</p>
+                </div>
+                <div>
+                    <h5 class="fw-bold">Head Office</h5>
+                    <p>Jl. Telekomunikasi No. 1, Terusan Buahbatu - Bojongsoang, Sukapura, Kec. Dayeuhkolot,
+                        Kabupaten Bandung,
+                        Jawa Barat 40257</p>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <h5 class="fw-bold">Company</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a class="text-decoration-none text-black-50" href="">About HiFarm</a></li>
+                    <li class="mb-2"><a class="text-decoration-none text-black-50" href="">Blog & News</a></li>
+                    <li class="mb-2"><a class="text-decoration-none text-black-50" href="">Partnership</a></li>
+                    <li class="mb-2"><a class="text-decoration-none text-black-50" href="">Community Development</a></li>
+                    <li class="mb-2"><a class="text-decoration-none text-black-50" href="">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <h5 class="fw-bold">Contact Us</h5>
+                <div class="mb-3 bd-highlight d-flex justify-content-between">
+                    <a class="me-2" href=""><img
+                            src="https://susunjadwal.cs.ui.ac.id/static/media/facebook.a81cc3b6.svg" alt=""></a>
+                    <a class="me-2" href=""><img
+                            src="https://susunjadwal.cs.ui.ac.id/static/media/intagram.2d9aea33.svg" alt=""></a>
+                    <a class="me-2" href=""><img src="https://susunjadwal.cs.ui.ac.id/static/media/line.90148e0f.svg"
+                            alt=""></a>
+                    <a class="me-2" href=""><img
+                            src="https://susunjadwal.cs.ui.ac.id/static/media/linkedin.4dcf2346.svg" alt=""></a>
+                    <a class="me-2" href=""><img src="https://susunjadwal.cs.ui.ac.id/static/media/medium.b23a815e.svg"
+                            alt=""></a>
+                    <a class="me-2" href=""><img src="https://susunjadwal.cs.ui.ac.id/static/media/twitter.0d0c0920.svg"
+                            alt=""></a>
+                </div>
+            </div>
+        </div>
+        <p>© 2022 HIFARM</p>
+    </div>
+</footer>
---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <x-sidebar-navigation/>
+
+        <div class="col-lg-10">
+            <div class="card">
+                <div class="card-header">{{ __('Buat Produk') }}</div>
+                <div class="card-body">
+                    <form action="" method="">
+                        @csrf
+
+                        <div class="mb-3 row">
+                            <label for="gambar" class="col-sm-2 col-form-label">{{ __('Gambar Produk') }}</label>
+                            <div class="col-sm-10">
+                                <input class="form-control" type="file" id="gambar" name="gambar">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="nama" class="col-sm-2 col-form-label">{{ __('Nama Produk') }}</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="nama" name="nama">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="harga" class="col-sm-2 col-form-label">{{ __('Harga Produk') }}</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id="harga" name="harga" min="1">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="berat" class="col-sm-2 col-form-label">{{ __('Berat Produk') }}</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="berat" name="berat">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="kategori" class="col-sm-2 col-form-label">{{ __('Kategori Produk') }}</label>
+                            <div class="col-sm-10">
+
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="kategori" id="inlineRadio1"
+                                        value="Hewan Ternak">
+                                    <label class="form-check-label" for="inlineRadio1">{{ __('Hewan Ternak') }}</label>
+                                </div>
+
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="kategori" id="inlineRadio2"
+                                        value="Produk Ternak">
+                                    <label class="form-check-label" for="inlineRadio2">{{ __('Produk Ternak') }}</label>
+                                </div>
+
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="kategori" id="inlineRadio3"
+                                        value="Pakan Ternak">
+                                    <label class="form-check-label" for="inlineRadio3">{{ __('Pakan Ternak') }}</label>
+                                </div>
+
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="kategori" id="inlineRadio4"
+                                        value="Lainnya">
+                                    <label class="form-check-label" for="inlineRadio4">{{ __('Lainnya') }}</label>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="keterangan" class="col-sm-2 col-form-label">{{ __('Keterangan') }}</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <a class="btn btn-danger me-md-2" href="{{ route('product.index') }}">{{ __('Cancel') }}</a>
+                            <button class="btn btn-primary" type="submit">{{ __('Submit') }}</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
