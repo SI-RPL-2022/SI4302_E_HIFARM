959fab6794cfd0b8e46c388563c076c483267d5d
icarryu <zulf6447@gmail.com>
2022-04-01 08:41:29 +0700

    [Add: File Landing Page ref #2]

---
+++ resources/views/tes.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container ">
+      <div class="card mb-5">
+        <img
+          src="https://images.unsplash.com/photo-1594302954323-7846bf62b5a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2FwaXxlbnwwfHwwfHw%3D&w=1000&q=80"
+          height="350" class="card-img-top" alt="...">
+      </div>
+      <div class="row mb-5">
+        <div class="col-sm">
+          <h2 class="fw-bold">Menjadikan Ternak Sebagai Mata Pencaharian yang Lebih Baik untuk Indonesia</h2>
+          <p>HiFarm bertujuan untuk menghubungkan peternak skala kecil dengan eksposur global dalam mempromosikan produk
+            ternak Indonesia yang lezat; melalui kesepakatan grosir, belanja eceran, dan kemitraan bisnis. </p>
+        </div>
+        <div class="col-sm">
+          <img src="https://disnakkeswan.ntbprov.go.id/wp-content/uploads/2020/11/FB_IMG_1604063969568-732x380.jpg"
+          height="" class="card-img-top" alt="...">
+        </div>
+      </div>
+    </div>
+    <div class="mb-5 py-4" style="background-color: #FDE57A;">
+      <h3 class="text-center mb-4">Bergabung Dalam Ekosistem HiFarm</h3>
+      <div class="container row mx-auto">
+        <div class="col-sm mb-2">
+          <img src="https://disnakkeswan.ntbprov.go.id/wp-content/uploads/2020/11/FB_IMG_1604063969568-732x380.jpg"
+          height="" class="card-img-top" alt="...">
+        </div>
+        <div class="col-sm mb-2">
+          <img src="https://disnakkeswan.ntbprov.go.id/wp-content/uploads/2020/11/FB_IMG_1604063969568-732x380.jpg"
+          height="" class="card-img-top" alt="...">
+        </div>
+        <div class="col-sm mb-2">
+          <img src="https://disnakkeswan.ntbprov.go.id/wp-content/uploads/2020/11/FB_IMG_1604063969568-732x380.jpg"
+          height="" class="card-img-top" alt="...">
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="text-center">
+          <p class="fs-5">HiFarm membuka kesempatan untuk kamu para <br> peternak yang ingin terlibat untuk memanjukan
+            <br> Peternakan Indonesia.</p>
+          <a class="btn btn-primary" href="#" role="button">Gabung Sekarang</a>
+        </div>
+      </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/test', [App\Http\Controllers\HomeController::class, 'tes'])->name('test');
+Route::get('/', [App\Http\Controllers\FrontController::class, 'landingpage'])->name('landingpage');
