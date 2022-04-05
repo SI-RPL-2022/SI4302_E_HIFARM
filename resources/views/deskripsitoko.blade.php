cd43722b56e4e8a99324e77ccdc35dccc4a6b184
fajribryan <mbryanpratama@gmail.com>
2022-04-05 17:15:08 +0700

    mengelola informasi toko rev #3

--- resources/views/deskripsitoko.blade.php
+++ resources/views/deskripsitoko.blade.php
@@ -97,49 +97,16 @@
     }
 
 </style>
-<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <svg class="bd-placeholder-img" width="100%"  height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-<img src="https://resepkoki.co/wp-content/uploads/2013/06/ayam-goreng-kfc.jpg" class="d-block w-100"  alt="">
-        <div class="container">
-          <div class="carousel-caption text-start">
-            <h1>Toko Pa budi</h1>
-            <p>pa budi jaya jaya jaya jaya</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Kunjungi toko</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-<img src="https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/11/27/2455117790.jpeg" alt="">
-        <div class="container">
-          
-        </div>
-      </div>
-      <div class="carousel-item">
-        <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-<img src="https://image.akurat.co/images/uploads/images/akurat_20190507094558_nwaSFH.jpg" alt="">
-        <div class="container">
-          
-        </div>
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
+<div class="p-5 mb-4 bg-light rounded-3">
+<div class="col">
 
+  <div class="container-fluid py-5">
+    
+    <h1 class="display-5 fw-bold">Toko Abadi</h1>
+    <p class="col-md-8 fs-4">Menyediakan berbagai daging terbaik</p>
+  </div>
+</div>
+  </div>
   <div class="container">
 			<!-- row 1 -->
 			<div class="row mt-2 mb-4">
