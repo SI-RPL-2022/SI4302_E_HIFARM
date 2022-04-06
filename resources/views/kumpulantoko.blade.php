49fc3459c39b3c59b9b01d45c9e56652dc4b12c9
brianajaaa <febrianfahmi.a@gmail.com>
2022-04-06 20:56:48 +0700

    [Add: File Kumpulan Toko ref #5]

---
+++ resources/views/kumpulantoko.blade.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style_semuaToko.css">
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Semua Toko</title>
+	</head>
+
+	<body>
+		
+		<!-- form -->
+		<div class="container"><br><br><br>
+			<h1 class="text-center mt-5 mb-5">Semua Toko</h1>
+			<div class="custom-inpForm text-center">
+			 	<input type="email" class="form-control form-control-lg" id="exampleFormControlInput1" placeholder="Search">
+
+			 	<a href="">
+			 	<div class="filter-icon">
+			 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M3.853 54.87C10.47 40.9 24.54 32 40 32H472C487.5 32 501.5 40.9 508.1 54.87C514.8 68.84 512.7 85.37 502.1 97.33L320 320.9V448C320 460.1 313.2 471.2 302.3 476.6C291.5 482 278.5 480.9 268.8 473.6L204.8 425.6C196.7 419.6 192 410.1 192 400V320.9L9.042 97.33C-.745 85.37-2.765 68.84 3.854 54.87L3.853 54.87z"/></svg>
+			 	</div>
+			 </a>
+			 </div>
+
+			<!-- row 1 -->
+			<div class="row mb-3">
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>	
+				</div>
+
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>		
+				</div>
+			</div>
+
+			<!-- row 2 -->
+			<div class="row mb-3">
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>	
+				</div>
+
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</div>
+			</div>
+
+			<!-- row 3 -->
+			<div class="row mb-3">
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>	
+				</div>
+
+				<div class="col">
+					<div class="card mb-5" style="max-width: 540px; border-radius: 20px;">
+					  <div class="row g-0">
+					    <div class="col-md-4">
+					      <img src="assets/discord3.png" class="img-fluid rounded-start" alt="...">
+					    </div>
+					    <div class="col-md-8">
+					      <div class="card-body">
+					        <h5 class="card-title">Nama Toko</h5>
+					        <p class="card-text">Jl. TelekomunikasiNo. 1, Terusan Buahbatu - BojongsoangJl. TelekomunikasiNo. 1, Terusan Buahbatu - Bojongsoang</p>
+					        <p class="card-text"><a href="#" style="text-decoration: none; font-weight: 450; font-size: 18px;">Kunjungi Toko ></a></p>
+					      </div>
+					    </div>
+					  </div>
+					</div>					
+				</div>
+			</div>
+
+
+
+			<div class="pagination-position">
+					    <nav aria-label="Page navigation example">
+						  <ul class="pagination">
+						    <li class="page-item">
+						      <a class="page-link" href="#" aria-label="Previous">
+						        <span aria-hidden="true">&laquo;</span>
+						      </a>
+						    </li>
+						    <li class="page-item"><a class="page-link" href="#">1</a></li>
+						    <li class="page-item"><a class="page-link" href="#">2</a></li>
+						    <li class="page-item"><a class="page-link" href="#">3</a></li>
+						    <li class="page-item">
+						      <a class="page-link" href="#" aria-label="Next">
+						        <span aria-hidden="true">&raquo;</span>
+						      </a>
+						    </li>
+						  </ul>
+						</nav>
+					</div>
+		</div><br><br><br>
+
+	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/createlaman', function () {
-    return view('createlaman');
+    return view('kumpulantoko');
 });
