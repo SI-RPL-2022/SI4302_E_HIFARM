f98f81f54205d5de02204eb4dd28db5bfa2184fc
dzzlr <githubdazzler@gmail.com>
2022-05-13 06:59:34 +0700

    [Add: File Routes for Blog ref #7]

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
