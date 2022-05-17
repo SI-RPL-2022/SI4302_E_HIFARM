8b60cdeacea585787510d457af3d2baf4ddbdc4a
icarryu <zulf6447@gmail.com>
2022-05-17 11:58:52 +0700

    [Add : File pembelian produk ref #8]

--- resources/views/pembelian.blade.php
+++ resources/views/pembelian.blade.php
@@ -1,24 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<!doctype html>
-<html lang="en">
-	<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="assets/css/style_header.css">
-	<link rel="stylesheet" href="assets/css/style_footer.css">
-	<link rel="stylesheet" href="assets/css/style_detailProdukMember.css">
-
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<title>Detail Produk</title>
-	</head>
-
-	<body>
-		
-
 		<!-- form -->
 		<div class="container">
 			<div class="row">
@@ -102,10 +84,4 @@
 				</div>
 			</div>
 		</div>
-
-		
-	<script src="assets/js/sticky_vanila_toggle.js"></script>
-
-	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	</body>
-</html>
+@endsection
